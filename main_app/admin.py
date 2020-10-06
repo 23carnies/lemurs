791fde3f565e28c9d4f5bd4a1a6a8f70390641ae
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-06 15:06:36 -0400

    Lemur detail functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Lemur
 
 # Register your models here.
+admin.site.register(Lemur)
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Lemur:
-    def __init__(self, name, species, description, age):
-        self.name = name
-        self.species = species
-        self.description = description
-        self.age = age
-
-lemurs = [
-    Lemur('Zoboomafoo', 'ring-tailed', 'precocious', 4),
-    Lemur('Jovian', 'ring-tailed', 'shy', 10),
-    
-]
+from .models import Lemur
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Wow! Lemurs!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def lemurs_index(request):
+    lemurs = Lemur.objects.all()
     return render(request, 'lemurs/index.html', { 'lemurs': lemurs })
+
+def lemurs_detail(request, lemur_id):
+    lemur = Lemur.objects.get(id=lemur_id)
+    return render(request, 'lemurs/detail.html', { 'lemur': lemur })
