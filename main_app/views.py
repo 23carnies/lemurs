35003cef00869b8a9784866fa0d8669f244d7742
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-06 13:17:16 -0400

    Set up lemur view

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Lemur:
+    def __init__(self, name, species, description, age):
+        self.name = name
+        self.species = species
+        self.description = description
+        self.age = age
+
+lemurs = [
+    Lemur('Zoboomafoo', 'ring-tailed', 'precocious', 4),
+    Lemur('Jovian', 'ring-tailed', 'shy', 10),
+    
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Wow! Lemurs!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Lemurs</h1>')
+    return render(request, 'about.html')
+
+def lemurs_index(request):
+    return render(request, 'lemurs/index.html', { 'lemurs': lemurs })
