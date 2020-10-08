741a0fa01e54c8a1a6821581ef35108bf3b1677d
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-08 11:33:07 -0400

    Add toy urls and view functions

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Lemur, Feeding
+from .models import Lemur, Feeding, Toy
 
 # Register your models here.
 admin.site.register(Lemur)
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('lemurs/<int:pk>/update/', views.LemurUpdate.as_view(), name='lemurs_update'),
     path('lemurs/<int:pk>/delete/', views.LemurDelete.as_view(), name='lemurs_delete'),
     path('lemurs/<int:lemur_id>/add_feeding', views.add_feeding, name='add_feeding'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/<int:pk/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('cats/<int:cat_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
 ]
